85b1ee0bf750fb86eabe47788ae7c8277dade0bc
TrixerOG <ilia.vialtsev@yandex.ru>
2024-05-19 18:13:38 +0300

    Test5

--- school_library/catalog/models.py
+++ school_library/catalog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -9,10 +8,11 @@ class Author(models.Model):
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
 
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    summary = models.TextField(max_length=1000, blank=True, null=True)
+    isbn = models.CharField('ISBN', max_length=13, unique=True)
 
     def __str__(self):
         return self.title
